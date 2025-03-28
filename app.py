dc1d0b1a266a506c34574350b270b66279ca1fd3
frankcode04 <unogrupo191@gmail.com>
2025-03-28 08:38:46 -0500

    init

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, send_file
+import qrcode
+from io import BytesIO
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/generate_qr', methods=['POST'])
+def generate_qr():
+    data = request.form['student_code']
+    img = qrcode.make(data)
+    
+    img_io = BytesIO()
+    img.save(img_io, 'PNG')
+    img_io.seek(0)
+    
+    return send_file(img_io, mimetype='image/png')
+
+if __name__ == '__main__':
+    app.run(debug=True)
